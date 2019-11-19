8589f7416b5873833b30679b7516faae3248966b
Chris Masters <cmas123456@gmail.com>
2019-11-19 08:49:58 -0800

    merge

--- main.js
+++ main.js
@@ -11,33 +11,64 @@ let isForward = true;
 
 //create bricks
 const invaderArray = []
-for (let index = 1; index <= 5; index++){
+
+for (let row = 1; row <= 5; row++){
+  for (let alien = 1; alien <= 11; alien++){
+    let rowArray = [];
     const invader = {
-        origin: [
-            ((window.innerWidth / 10) * index ) - ((window.innerWidth / 20) / 2),
-			(window.innerHeight / 20)
-        ],
-        health: 2,
-        dimensions: [
-            window.innerWidth / 20,
-            window.innerHeight / 40
-        ],
-        color: () => {
-          switch(invader.health) {
+      origin: [
+        ((window.innerWidth / 11) * alien ) - ((window.innerWidth / 22) / 2),
+        ((window.innerHeight / 20) + (row * 10))
+      ],
+      health: 2,
+      dimensions: [
+        window.innerWidth / 10,
+        window.innerHeight / 20,
+      ],
+      color: () => {
+        switch(invader.health){
           case 2:
             return 'green'
-          case 1: 
+          case 1:
+            return 'yellow'
+          case 0: 
             return 'red'
-          case 0:
-            return 'black'
           default:
-          return 'green'
-
-          }
+            return 'green'
         }
-   }
-    invaderArray.push(invader);
+      }
+    }
+    rowArray.push(invader);
+  }
+  invaderArray.push(rowArray);
 }
+// for (let index = 1; index <= 5; index++){
+//     const invader = {
+//         origin: [
+//             ((window.innerWidth / 10) * index ) - ((window.innerWidth / 20) / 2),
+// 			(window.innerHeight / 20)
+//         ],
+//         health: 2,
+//         dimensions: [
+//             window.innerWidth / 20,
+//             window.innerHeight / 40
+//         ],
+//         color: () => {
+//           switch(invader.health) {
+//           case 2:
+//             return 'green'
+//           case 1: 
+//             return 'red'
+//           case 0:
+//             return 'black'
+//           default:
+//           return 'green'
+
+//           }
+//         }
+//    }
+//   invaderArray.push(invader);
+// }
 
 setInterval(() => {
     context.clearRect(0, 0, window.innerWidth, window.innerHeight);
