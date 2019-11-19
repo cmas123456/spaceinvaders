573cfb78d285d65624d786fd2d8ebd2f174bcfdd
Chris Masters <cmas123456@gmail.com>
2019-11-19 10:07:31 -0800

    11 x 5 aliens moving downward

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
 const source = document.createElement('canvas')
 assignAttributes(source, {
-	id: 'source',
+  id: 'source',
 	height: window.innerHeight,
 	width: window.innerWidth
 })
 document.body.appendChild(source)
 
 const context = source.getContext('2d', {alpha: 'false'})
-let isForward = true;
+let changeDirection = false;
 
 //create bricks
 const invaderArray = []
 
 for (let row = 1; row <= 5; row++){
+  let rowArray = []
   for (let alien = 1; alien <= 11; alien++){
-    let rowArray = [];
     const invader = {
       origin: [
-        ((window.innerWidth / 11) * alien ) - ((window.innerWidth / 22) / 2),
-        ((window.innerHeight / 20) + (row * 10))
+        (((window.innerWidth / 20) * alien) - (window.innerWidth / 40) / 2),
+        ((window.innerHeight / 20) + (row * 30))
       ],
       health: 2,
       dimensions: [
-        window.innerWidth / 10,
-        window.innerHeight / 20,
+        window.innerWidth / 60,
+        window.innerHeight / 99,
       ],
       color: () => {
         switch(invader.health){
@@ -36,7 +36,8 @@ for (let row = 1; row <= 5; row++){
           default:
             return 'green'
         }
-      }
+      },
+      isForward: true
     }
     rowArray.push(invader);
   }
@@ -78,31 +79,54 @@ setInterval(() => {
     context.fillRect(0, window.innerHeight - (window.innerHeight / 10),
     window.innerWidth - 50, window.innerHeight / 10);
     
-invaderArray.forEach(ele => {
-        context.fillStyle = ele.color();
-        context.fillRect(ele.origin[0], ele.origin[1], ele.dimensions[0], ele.dimensions[1])
-        if (isForward){
-        	ele.origin[0] += 10; 
-        }
-        else if (!isForward){
-        	ele.origin[0] -= 10; 
-        }
-         
-})
-invaderArray.forEach(ele => {
-  if (ele.origin[0] < 10){
-        	isForward = true;
-        	invaderArray.forEach(ele => {
-        	ele.origin[1] += 10;
-        	})
-        }
-        if (ele.origin[0] > window.innerWidth - 100){
-        	isForward = false;
-        	invaderArray.forEach(ele => {
-        	ele.origin[1] += 10;
-        	})
-        }
+invaderArray.forEach(row  => {
+  row.forEach(alien => {
+    context.fillStyle = alien.color();
+    context.fillRect(alien.origin[0], alien.origin[1], alien.dimensions[0], alien.dimensions[1])
+    if (alien.isForward){
+      alien.origin[0] += 10;
+      if (alien.origin[0] > window.innerWidth - 100){
+        willFlip = true;
+      }
+    }
+    if (!alien.isForward){
+      alien.origin[0] -= 10;
+      if (alien.origin[0] < 10) {
+        willFlip = true;
+      } 
+    }
   })
+})
+
+invaderArray.forEach(row => {
+  row.forEach(alien => {
+    if (willFlip === true && alien.isForward === true){
+      alien.isForward = false;
+      alien.origin[1] += 10;
+    }
+    else if (willFlip === true && alien.isForward === false){
+      alien.isForward = true;
+      alien.origin[1] += 10;
+    }
+
+  })
+})
+willFlip = false;
+// })
+// invaderArray.forEach(ele => {
+//         if (ele.origin[0] < 10){
+//         	isForward = true;
+//         	invaderArray.forEach(ele => {
+//         	ele.origin[1] += 10;
+//         	})
+//         }
+//         if (ele.origin[0] > window.innerWidth - 100){
+//         	isForward = false;
+//         	invaderArray.forEach(ele => {
+//         	ele.origin[1] += 10;
+//         	})
+//         }
+  // }
 },100)
     
 
